f31e3fee1d208100992131fbaa6a94225c44d7b6
Nojaid Ad <nojaidnojaid@gmail.com>
2025-12-13 12:27:17 +0200

    update project adding js light mode dark mode drop down menu

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,31 +1,38 @@
-var isDarkMode = false;
+var isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
 var isMenuOpen = false;
-function navClick(){
 
-  const navigation = document.querySelector(".navigation");
+function initTheme() {
+  const iconTheme = document.getElementById("iconTheme");
 
-  navigation.classList.toggle("active");
+  if (isDarkMode) {
+    document.body.classList.add("dark-mode");
+    iconTheme.className = "fa-solid fa-moon";
+  } else {
+    document.body.classList.remove("dark-mode");
+    iconTheme.className = "fa-solid fa-sun";
+  }
 }
+
+function navClick() {
+  openMenu();
+}
+
 function openMenu() {
-  if (window.innerWidth <= 768)
-  {
-    navClick();
+  if (window.innerWidth <= 768) {
+    const navigation = document.querySelector(".navigation");
+    navigation.classList.toggle("active");
   }
 
   isMenuOpen = !isMenuOpen;
 
   const menuIcon = document.getElementById("menuIcon");
   menuIcon.className = isMenuOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+
   document.getElementById("logo").style.display = isMenuOpen
     ? "none"
     : "inline";
 }
 function toggleTheme() {
   isDarkMode = !isDarkMode;
-
-  const iconTheme = document.getElementById("iconTheme");
-
-  iconTheme.className = isDarkMode ? "fa-solid fa-moon" : "fa-solid fa-sun";
-
-  document.body.classList.toggle("dark-mode");
+  initTheme();
 }
